a5bd4e0f90309aaf302d133a1b28890e19d67a1d
LeWilSy <b.tighidet0@gmail.com>
2024-01-22 10:21:30 +0100

    CARROUSEL 3 IMG FINISH

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,17 +1,20 @@
+const arrows = document.querySelectorAll(".arrow")
+const customers = document.querySelectorAll(".customer")
 const Dates = new Date();
 const Year = Dates.getFullYear();
 
-document.addEventListener("click", (e) => {
-  console.log(e);
-  if (e.target === prev) {
-    customer1.classList.toggle("active");
-    customer2.classList.toggle("active");
-  }
-  if (e.target === next) {
-    customer2.classList.toggle("active");
-    customer1.classList.toggle("active");
-  }
-});
+arrows.forEach((arrow) => {
+  arrow.addEventListener('click', (e) => {
+    const nextCustomer = e.target.id === "next" ? 1 : -1;
+    const customerActive = document.querySelector(".active");
 
-meta.innerText =
-  "Actif Services Nettoyage © " + Year + " - Tous droits réservés";
+    newActive = nextCustomer  + [...customers].indexOf(customerActive);
+    if (newActive < 0 ) newActive = [...customers].length - 1;
+    if (newActive >= [...customers].length ) newActive = 0;
+    customers[newActive].classList.add("active");
+    customerActive.classList.remove("active")
+  })
+} )
+
+meta.innerText = "Actif Services Nettoyage © " + Year + " - Tous droits réservés";
+
