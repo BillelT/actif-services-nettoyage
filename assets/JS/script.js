221532822fc153cd131bcdfcbb5698de041076b0
LeWilSy <b.tighidet0@gmail.com>
2024-02-01 23:26:51 +0100

    HOMEPAGE CARROUSEUL FINISH

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,8 +1,10 @@
 const arrows = document.querySelectorAll(".arrow");
+const slides = document.querySelectorAll(".slides");
+const bullets = document.querySelectorAll(".bullet");
 const customers = document.querySelectorAll(".customer");
 const Dates = new Date();
 const Year = Dates.getFullYear();
-const navLinks = ["Contrats", "Prestations", "Contact","Devis"];
+const navLinks = ["Contrats", "Prestations", "Contact", "Devis"];
 
 //make the header fixed to the top after 3 scrolls from the user
 window.addEventListener("scroll", (e) => {
@@ -13,13 +15,13 @@ window.addEventListener("scroll", (e) => {
     header.style.background = "#fefffe";
     header.style.paddingBottom = "8px";
     header.style.paddingTop = "8px";
-    navMobile.style.top ="58.8px"
+    navMobile.style.top = "58.8px"
     trust.style.paddingTop = "202.8px"
-  } else if(mobileMenu.innerText !== "X") {
+  } else if (mobileMenu.innerText !== "X") {
     header.style.paddingBottom = "16px";
     header.style.paddingTop = "16px";
     header.style.position = "relative";
-    navMobile.style.top ="68.8px"
+    navMobile.style.top = "68.8px"
     header.style.background = "transparent";
     trust.style.paddingTop = "128px"
   }
@@ -30,7 +32,7 @@ arrows.forEach((arrow) => {
   arrow.addEventListener("click", (e) => {
     const nextCustomer = e.target.id === "next" ? 1 : -1;
     const customerActive = document.querySelector(".active");
-
+    
     newActive = nextCustomer + [...customers].indexOf(customerActive);
     if (newActive < 0) newActive = [...customers].length - 1;
     if (newActive >= [...customers].length) newActive = 0;
@@ -39,10 +41,47 @@ arrows.forEach((arrow) => {
   });
 });
 
+// carrousel navigation under 590px width device
+slides.forEach((slide) => {
+  slide.addEventListener("touchstart", (e) => {
+    touchStartX = e.touches[0].clientX;
+  })
+  slide.addEventListener("touchend", (e) => {
+    touchEndX = e.changedTouches[0].clientX;
+    const swipeDistance = touchStartX - touchEndX;
+    
+    const nextCustomer = swipeDistance > 0  ? 1 : -1;
+    const customerActive = document.querySelector(".active");
+    
+    newActive = nextCustomer + [...customers].indexOf(customerActive);
+    if (newActive < 0) newActive = [...customers].length - 1;
+    if (newActive >= [...customers].length) newActive = 0;
+    customers[newActive].classList.add("active");
+    customerActive.classList.remove("active");
+    console.log(newActive);
+
+    bullets.forEach((bullet) => {
+      bullet.style.transition = "0.4s ease-in-out";
+      if (newActive === 0) {
+        bullet.classList.remove("bulletActive");
+        bullets[0].classList.add("bulletActive");
+      }
+      if (newActive === 1) {
+        bullet.classList.remove("bulletActive");
+        bullets[1].classList.add("bulletActive");
+      }
+      if (newActive === 2) {
+        bullet.classList.remove("bulletActive");
+        bullets[2].classList.add("bulletActive");
+      }
+    })
+  })
+});
+
+
 
 // Burger Menu
 document.addEventListener("click", (e) => {
-  console.log(e);
   if (e.target.innerText === "☰") {
     navMobile.classList.toggle("mobile-nav-clicked");
     mobileMenu.innerText = "X";
